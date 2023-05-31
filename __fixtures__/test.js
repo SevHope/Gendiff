6d41ac6c1fcb970dcdc6e94ad4150bb37fdbcb71
Nadezhda Sevastianova <s.hope@bk.ru>
2023-05-31 22:10:27 +0300

    update tests

--- __fixtures__/test.js
+++ __fixtures__/test.js
@@ -1,5 +1,6 @@
+import * as fs from 'node:fs';
+import { fileURLToPath } from 'url';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import fs from 'fs';
 import getDifference from '../src/genDiff.js';
 
@@ -8,11 +9,10 @@ const generateDName = path.dirname(importFileName);
 
 const getFixturePath = (filename) => path.join(generateDName, '..', '__fixtures__', filename);
 
-const test = ('JSON file', () => {
+test('JSON file', () => {
   const filename1 = getFixturePath('file1.json');
   const filename2 = getFixturePath('file2.json');
   const resultFilename = getFixturePath('fileResult.txt');
   const result = fs.readFileSync(resultFilename, 'utf-8');
   expect(getDifference(filename1, filename2)).toBe(result);
 });
-export default test;
