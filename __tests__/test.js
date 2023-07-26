66e510125af97e55d26392d82bf975f9bb65698c
Nadezhda Sevastianova <s.hope@bk.ru>
2023-07-26 16:16:47 +0300

    delete fixtures and tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,19 +1,61 @@
-import { readFileSync } from 'node:fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getActual = (filename1, filename2, format) => {
+  const path1 = getFixturePath(filename1);
+  const path2 = getFixturePath(filename2);
+  return genDiff(path1, path2, format);
+};
 
-const result = readFile('resultJSON.txt');
+const cases = [
+  {
+    file1: 'file3.json',
+    file2: 'file4.json',
+    exp: 'resultStylish.txt',
+    format: 'stylish',
+  },
+  {
+    file1: 'file3.json',
+    file2: 'file4.json',
+    exp: 'resultPlain.txt',
+    format: 'plain',
+  },
+  {
+    file1: 'file1.yaml',
+    file2: 'file2.yaml',
+    exp: 'resultStylish.txt',
+    format: 'stylish',
+  },
+  {
+    file1: 'file1.yaml',
+    file2: 'file2.yaml',
+    exp: 'resultPlain.txt',
+    format: 'plain',
+  },
+];
 
-test('genDiff', () => {
-  const filepath3 = getFixturePath('file3.json');
-  const filepath4 = getFixturePath('file4.json');
-  expect(genDiff(filepath3, filepath4)).toBe(result);
+test.each(cases)(
+  'genDiff should return the expected output for %p',
+  ({
+    file1, file2, exp, format,
+  }) => {
+    const actual = getActual(file1, file2, format);
+    const expected = readFile(exp);
+    expect(actual).toBe(expected);
+  },
+);
+
+test('test format JSON', () => {
+  const file1 = getFixturePath('file3.json');
+  const file2 = getFixturePath('file4.json');
+  const data = genDiff(file1, file2, 'json');
+  expect(() => JSON.parse(data)).not.toThrow();
 });
--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import genDiff from '../src/genDiff.js';
+import genDiff from '../index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
