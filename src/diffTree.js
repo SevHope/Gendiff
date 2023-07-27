f6d79485ef48532b6fb5babc7b2feb35d2dddd42
Nadezhda Sevastianova <s.hope@bk.ru>
2023-07-27 12:10:19 +0300

    update diftree

--- src/diffTree.js
+++ src/diffTree.js
@@ -7,14 +7,14 @@ const makeDiffTree = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, children: makeDiffTree(value1, value2), type: 'nested' };
-    }
     if (!_.has(data1, key)) {
       return { key, value: value2, type: 'added' };
     }
     if (!_.has(data2, key)) {
       return { key, value: value1, type: 'deleted' };
+    }
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return { key, children: makeDiffTree(value1, value2), type: 'nested' };
     }
     if (value1 !== value2) {
       return {
